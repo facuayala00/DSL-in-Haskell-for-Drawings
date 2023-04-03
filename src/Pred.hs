224aa97a6dd26db6fac5f2ba6197924383a425d3
Benjamin Lozano <benjalozano13@outlook.com>
2023-04-02 22:43:59 -0300

    Agregadas algunas cosas que estaban en el word

--- src/Pred.hs
+++ src/Pred.hs
@@ -3,21 +3,34 @@ module Pred (
   cambiar, anyDib, allDib, orP, andP
 ) where
 
+
+-- `Pred a` define un predicado sobre figuras básicas. Por ejemplo,
+-- `(== Triangulo)` es un `Pred TriOCuat` que devuelve `True` cuando la
+-- figura es `Triangulo`.
+
 type Pred a = a -> Bool
 
--- Dado un predicado sobre básicas, cambiar todas las que satisfacen
--- el predicado por la figura básica indicada por el segundo argumento.
+-- Dado un predicado sobre figuras básicas, cambiar todas las que satisfacen
+-- el predicado por el resultado de llamar a la función indicada por el
+-- segundo argumento con dicha figura.
+-- Por ejemplo, `cambiar (== Triangulo) (\x -> Rotar (Figura x))` rota
+-- todos los triángulos.
+cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
 cambiar = undefined
 
 
--- Alguna básica satisface el predicado.
+-- Alguna figura satisface el predicado.
+anyFig :: Pred a -> Dibujo a -> Bool
 anyDib = undefined
 
--- Todas las básicas satisfacen el predicado.
+-- Todas las figuras satisfacen el predicado.
+allFig :: Pred a -> Dibujo a -> Bool
 allDib = undefined
 
 -- Los dos predicados se cumplen para el elemento recibido.
+andP :: Pred a -> Pred a -> Pred a
 andP = undefined
 
 -- Algún predicado se cumple para el elemento recibido.
+orP :: Pred a -> Pred a -> Pred a
 orP = undefined
