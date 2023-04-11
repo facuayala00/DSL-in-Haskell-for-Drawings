34d192f21efca92b9d1423bfda7c44707a4fb708
gastonBon <gastonbonfils@gmail.com>
2023-04-11 17:45:48 -0300

    se dibuja escher pero fiero

--- src/Dibujos/Escher.hs
+++ src/Dibujos/Escher.hs
@@ -2,7 +2,7 @@ module Dibujos.Escher (
     escherConf
 ) where
 
-import Graphics.Gloss (Picture, blue, color, line, pictures, red, white, polygon)
+import Graphics.Gloss (Picture, blue, color, line, pictures, red, white, polygon, blank)
 
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
 
@@ -10,8 +10,17 @@ import Dibujo
 import FloatingPic 
 import Interp (Conf(..), interp)
 
--- Supongamos que eligen.
-type Escher = Bool
+data Basica = Triangulo | Blank
+    deriving (Show, Eq)
+
+type Escher = Basica
+
+interpBas :: Output Basica
+interpBas Blank a b c = blank
+interpBas Triangulo a b c = pictures [line $ triangulo a b c, cara a b c]
+  where
+      triangulo a b c = map (a V.+) [zero, c, b, zero]
+      cara a b c = polygon $ triangulo (a V.+ half c) (half b) (half c)
 
 -- El dibujoU.
 dibujoU :: Dibujo Escher -> Dibujo Escher
@@ -28,14 +37,14 @@ dibujoT p = encimar (p) (encimar p2 p3)
 
 -- Esquina con nivel de detalle n en base a la figura p.
 esquina :: Int -> Dibujo Escher -> Dibujo Escher
-esquina 0 _ = blank
-esquina n p = cuarteto (esquina (n-1) p) (lado (n-1) p) (rotar (lado (n-1) p)) dibujoU p
+esquina 0 _ = (figura Blank)
+esquina n p = cuarteto (esquina (n-1) p) (lado (n-1) p) (rotar (lado (n-1) p)) (dibujoU p)
 
 
 -- Lado con nivel de detalle.
 lado :: Int -> Dibujo Escher -> Dibujo Escher
-lado 0 _ = blank
-lado n p = cuarteto (lado (n-1) p ) (lado (n-1) p) (rotar (dibujoT p)) dibujoT p 
+lado 0 _ = (figura Blank)
+lado n p = cuarteto (lado (n-1) p ) (lado (n-1) p) (rotar (dibujoT p)) (dibujoT p) 
 
 -- Por suerte no tenemos que poner el tipo!
 noneto p q r s t u v w x = apilar 1 2 (juntar 1 2 p (juntar 1 1 q r)) 
@@ -43,7 +52,12 @@ noneto p q r s t u v w x = apilar 1 2 (juntar 1 2 p (juntar 1 1 q r))
                            (juntar 1 2 v (juntar 1 1 w x)))
 -- El dibujo de Escher:
 escher :: Int -> Escher -> Dibujo Escher
-escher n p = noneto (esquina n p) (lado n p) (r270 (esquina n p))
-                    (rotar (lado n p)) (dibujoU p) (r270 (lado n p))
-                    (rotar (esquina n p)) (r180 (lado n p)) (r180 (esquina n p))
+escher n p = noneto (esquina n (figura p)) (lado n (figura p)) (r270 (esquina n (figura p)))
+                    (rotar (lado n (figura p))) (dibujoU (figura p)) (r270 (lado n (figura p)))
+                    (rotar (esquina n (figura p))) (r180 (lado n (figura p))) (r180 (esquina n (figura p)))
 
+escherConf :: Conf
+escherConf = Conf {
+    name = "Escher",
+    pic = interp interpBas (escher 5 Triangulo)
+}
