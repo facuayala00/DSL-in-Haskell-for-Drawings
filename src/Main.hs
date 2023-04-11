f3b83197f7f50188a84b504be3e4b2dc2c8f1ee8
gastonBon <gastonbonfils@gmail.com>
2023-04-11 16:59:24 -0300

    lo de --lista en el main anda

--- src/Main.hs
+++ src/Main.hs
@@ -9,7 +9,7 @@ import Interp (Conf(name), initial)
 import Dibujos.Ejemplo (ejemploConf)
 import Dibujos.Feo (feoConf)
 import Dibujos.Grilla (grillaConf)
-import Dibujos.Escher (escherConf)
+-- import Dibujos.Escher (escherConf)
 
 -- Lista de configuraciones de los dibujos
 configs :: [Conf]
@@ -25,7 +25,30 @@ initial' (c : cs) n =
     else
         initial' cs n
 
+
+-- Función que imprime los nombres de los dibujos
+listarDibujos :: IO ()
+listarDibujos = do
+    putStrLn "Dibujos registrados:"
+    mapM_ (putStrLn . name) configs
+
+
+-- Muestra los posibles dibujos y le pide uno al usuario
+pedirDibujo :: IO ()
+pedirDibujo = do
+    listarDibujos
+    putStrLn "Ingrese el nombre del dibujo que desea dibujar:"
+    n <- getLine
+    initial' configs n
+
+-- Procesa los argumentos que se le pasa al main
+processArgs :: [String] -> IO ()
+processArgs args 
+    | null args = putStrLn "No se pasó ningún argumento."
+    | head args == "--lista" = pedirDibujo
+    | otherwise = initial' configs (head args)
+
 main :: IO ()
 main = do
     args <- getArgs
-    initial' configs $ head args
+    processArgs args
